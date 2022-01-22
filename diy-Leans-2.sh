25fb660a6a6ec1b7366dce700c62aa8983e889a6
xueyebai <zhongrenwei@gmail.com>
2022-01-22 14:43:07 +0800

    Update diy-Leans-2.sh

--- diy-Leans-2.sh
+++ diy-Leans-2.sh
@@ -16,3 +16,4 @@
 # Modify default theme
 #sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/+luci-theme-bootstrap /+luci-theme-argon  \\\n  +luci-app-openclash +luci-app-passwall  \\\n  +wget-nossl /g' feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-app-samba //g' package/lean/autosamba/Makefile
