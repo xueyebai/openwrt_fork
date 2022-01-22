ea33797dcaf3406072c864df2ba50bf0eb1831bf
xueyebai <zhongrenwei@gmail.com>
2022-01-22 18:48:52 +0800

    Update diy-part1-argon.sh

--- diy-part1-argon.sh
+++ diy-part1-argon.sh
@@ -22,4 +22,5 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feed
 cd lede/package/lean  
 rm -rf luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone  https://github.com/jerrykuku/luci-app-argon-config.git
 cd
