113b6373984ae34ad795e6665b0abfe8e8a74470
xueyebai <zhongrenwei@gmail.com>
2022-05-10 00:17:47 +0800

    Update diy-part2-Leans.sh

--- diy-part2-Leans.sh
+++ diy-part2-Leans.sh
@@ -15,4 +15,5 @@
 
 # Modify default theme
 #sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-#sed -i 's/+luci-theme-bootstrap /+luci-theme-bootstrap \\\n  +luci-app-ddns +luci-app-openclash +luci-app-passwall +luci-app-upnp +luci-app-wol \\\n  +luci-compat +luci-lib-ipkg +wget-nossl +wget-ssl +kmod-tun /g' feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-theme-bootstrap /+luci-theme-argon  \\\n  +luci-app-argon-config +luci-app-openclash +luci-app-passwall2 +luci-app-ddns  \\\n  +wget-nossl +luci-app-filetransfer /g' feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-app-samba //g' package/lean/autosamba/Makefile
